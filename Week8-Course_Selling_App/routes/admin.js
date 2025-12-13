b9c658d6975dd0b4d8e5411ca402929e0954e21a
tanayburbure <tanayburbure@gmail.com>
2025-12-13 22:31:04 +0530

    Admin routes updated

--- Week8-Course_Selling_App/routes/admin.js
+++ Week8-Course_Selling_App/routes/admin.js
@@ -57,15 +57,35 @@ adminRouter.post("/course", adminMiddleware, async function (req, res) {
     })
 })
 
-adminRouter.put("/course", function (req, res) {
+adminRouter.put("/course", adminMiddleware , async function (req, res) {
+    const adminId = req.userId;
+    const { title, description, imageUrl, price ,courseId} = req.body;
+
+
+
+    const course = await courseModel.updateOne({
+        _id : courseId,
+        creatorId : adminId
+    }
+        ,{
+        title, description, imageUrl, price
+    })
+
     res.json({
-        message: "This is put endpoint"
+        message: "course updated",
+        courseId: course._id
     })
 })
 
-adminRouter.get("/course/bulk", function (req, res) {
+adminRouter.get("/course/bulk",adminMiddleware ,async function (req, res) {
+    const adminId = req.userId;
+
+    const courses = await courseModel.find({
+        creatorId : adminId
+    })
     res.json({
-        message: "This get endpoint"
+        message: "This are all your courses",
+        courses
     })
 })
 
