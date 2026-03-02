5ae79300449570072c2bcf9004f37827bd851e98
tanayburbure <tanayburbure@gmail.com>
2026-03-02 14:27:22 +0530

    Added Recoil usage in react 18

--- Week11-Recoil/src/App.jsx
+++ Week11-Recoil/src/App.jsx
@@ -1,41 +1,53 @@
-import { useState } from 'react'
-import './App.css'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil';
+import { counterAtom } from './store/atom/counter';
+
+/// to use recoil we have to downgrade the version to 18...
 
 function App() {
-
   return (
-    <>
-    <Counter/>
-    </>
+    <RecoilRoot>
+      <Counter/>
+    </RecoilRoot>
   )
 }
 
 function Counter(){
-  const [count , setCount] = useState(0);
-
-  return <div>
-    {count}
-    <Increase setCount={setCount}/>
-    <Decrease setCount={setCount}/>
-  </div>
+  return (
+    <div>
+      <CurrentCounter/>
+      <Increase/>
+      <Decrease/>
+    </div>
+  )
 }
 
-function Increase({setCount}){
-  function increase(){
-    setCount(c => c + 1 )
-  }
-  return <div>
-    <button onClick={increase}>Increase</button>
-  </div>
-}
-function Decrease({setCount}){
-  function decrease(){
-    setCount(c => c + 1 )
-  }
-  return <div>
-    <button onClick={decrease}>Increase</button>
-  </div>
+function CurrentCounter(){
+  const count = useRecoilValue(counterAtom);
+  return <div>{count}</div>
 }
 
+function Increase(){
+  const setCount = useSetRecoilState(counterAtom);
 
-export default App
+  return (
+    <div>
+      <button onClick={() => setCount(c => c + 1)}>
+        Increase
+      </button>
+    </div>
+  )
+}
+
+function Decrease(){
+  const setCount = useSetRecoilState(counterAtom);
+
+  return (
+    <div>
+      <button onClick={() => setCount(c => c - 1)}>
+        Decrease
+      </button>
+    </div>
+  )
+}
+
+export default App;
