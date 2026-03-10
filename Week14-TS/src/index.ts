cd9323849fb92628085345c3359ee28aeb5e2d9e
tanayburbure <tanayburbure@gmail.com>
2026-03-10 21:27:06 +0530

    interfaces

--- Week14-TS/src/index.ts
+++ Week14-TS/src/index.ts
@@ -1,8 +1,8 @@
-function Greet(firstname : string){
-    return ("hello" + firstname)
-}
+// function Greet(firstname : string){
+//     return ("hello" + firstname)
+// }
 
-console.log(Greet("Sarthak "))
+// console.log(Greet("Sarthak "))
 
 
 
@@ -13,3 +13,24 @@ console.log(Greet("Sarthak "))
 // first have to convert into js file using npx tsc -b 
 // Then we can run node index.js
 // command to create .gitignore file ni .gitignore
+
+
+
+let firstname:string = "harkirat"
+let age : number = 21
+
+interface Usertype {
+    firstname : string ,
+    lastname : string,
+    age:number
+}
+
+function Greet(user : Usertype){
+
+}
+
+let user: Usertype = {
+    firstname : "harkirat",
+    lastname : "kirat",
+    age : 21,
+}
